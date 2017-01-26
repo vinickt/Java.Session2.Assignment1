47e005cf30da8c7a464eee8866e64424382def4d
vinickt <vini.ckt@gmail.com>
2017-01-26 14:23:04 +0530

    Add files via upload

---
+++ Print_Without_Semicolon.java
@@ -0,0 +1,12 @@
+
+public class Print_Without_Semicolon 
+{
+
+	  public static void main(String[] args) 
+	  {
+		  for(int i = 0;i<1;System.out.println("Hello Acadgild"))
+		  {
+			  i++;
+		  }
+	  }	  	    	      	  
+}
